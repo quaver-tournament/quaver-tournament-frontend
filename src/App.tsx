dae7cc8fc9bf86abe34674a909a80ee4cd41ccb4
Kevin-Lejava <klejava@gmail.com>
2022-03-07 23:27:59 -0500

    <QT-7> Created basic navbar template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
